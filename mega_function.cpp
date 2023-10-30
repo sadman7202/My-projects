33ffab28b0903fa392d6f5a60caaeafbf8418826
sadman7202 <royalsakib2@gmail.com>
2023-10-30 13:57:26 +0600

    Binary to decimal

--- mega_function.cpp
+++ mega_function.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+//f for prime 
 void prime (int x)
 {
    int i; 
@@ -14,6 +15,7 @@ void prime (int x)
     cout<<"Prime number"<<endl;
   }
 }
+//f for primeseries
   bool primesereis(int y)
 {
     for(int i=2;i<=sqrt(y);i++){
@@ -23,6 +25,7 @@ void prime (int x)
     }
     return true;
 }
+//f for fibonacci
 void fibonacci(int x)
 {
     int t1=0,t2=1,nextn;
@@ -34,6 +37,7 @@ void fibonacci(int x)
        t2=nextn;
     }
 }
+//f for factorial
 void factoral (int y)
 {
     int factorial=1;
@@ -44,18 +48,49 @@ void factoral (int y)
     cout<<"Your factorial is: "<<factorial<<endl;
     
 }
+//f for sumseries
+int sumseries(int x)
 
+{
+    int answer=0;
+    for (int i = 1; i <=x; i++)
+    {
+        answer=answer+i;
+
+    }
+    return answer;
+    
+}
+//f for d to binary
+void binarytodecimal(int n)
+{
+    int ans=0;
+    int x=1;
+    while (n>0)
+    {
+        int y=n%10;
+        ans+=x*y;
+        x*=2;
+        n/=10;
+
+    }
+    cout<<"your Binary number is : "<<ans<<endl;
+    
+}
     
 
 
 
 int main()
 {
+    cout<<"<<<<< Welcome to mega function >>>>>"<<endl;
     cout<<"Select what you wants to do: "<<endl;
     cout<<"1.Prime number"<<endl;
     cout<<"2.Prime number series"<<endl;
     cout<<"3.Fibonacci series"<<endl;
     cout<<"4.Factorial"<<endl;
+    cout<<"5.Sum of series"<<endl;
+    cout<<"Number conversions"<<endl;
     int i,a,b,n,p;
     cout<<"Enter your choice: ";
     cin>>n;
@@ -88,9 +123,37 @@ int main()
     cout<<"Enter number for factorail: "<<endl;     
     cin>>n;
     factoral(n);
+    case(5):
+    cout<<"Enter number for sum series: "<<endl;
+    cin>>n; 
+    cout<<"Your sum is: "<<sumseries(n)<<endl;
+    case(6):
+    cout<<"Select your choice"<<endl;
+    cout<<"1.Decimal to Binary"<<endl;
+    cout<<"2.Binary to Deciamal"<<endl;
+
+
+
+
+
+    int n;
+    cin>>n;
+    switch(n){
+        case(2):
+        int n;
+        cout<<"Enter your Binary number: "<<endl;
+        cin>>n;
+        binarytodecimal(n);
+
 
 
     }
+
+    
+
+
+    }
+    cout<<endl;
 
 
 
